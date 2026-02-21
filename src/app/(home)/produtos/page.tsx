3d1277f7083d75b23dc63e9fae5992b7a8632fbc
marcosmvf22 <marcosviniciuskta123@gmail.com>
2026-02-21 17:28:29 -0300

    style: remove linhas comentadas

--- src/app/(home)/produtos/page.tsx
+++ src/app/(home)/produtos/page.tsx
@@ -2,72 +2,7 @@ import { GetPVI } from "@/actions/PVI/actions";
 import Produtos from "@/src/components/produtos";
 
 export default async function ListaProdutos() {
-  // const cards = [
-  //   {
-  //     id: 1,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },
-  //   {
-  //     id: 2,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 3,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 4,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 5,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 6,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 7,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 8,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },{
-  //     id: 9,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },
-  //   {
-  //     id: 10,
-  //     image: "/logo/camisa1.png",
-  //     title: "Camiseta Confortável",
-  //     price: "R$ 49,90",
-  //     description: "Camiseta confortável e moderna",
-  //   },
-  // ];
-
+  
 const posts = await GetPVI()
 
   return (
