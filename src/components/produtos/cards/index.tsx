33b35e1cb826fbe926ce1c7db31840a1a1512382
marcosmvf22 <marcosviniciuskta123@gmail.com>
2026-02-22 20:24:34 -0300

    feat: adiciona limite de caracteres por titulo e descricao nos cards da pagina de produtos

--- src/components/produtos/cards/index.tsx
+++ src/components/produtos/cards/index.tsx
@@ -9,9 +9,12 @@ type CardProps = {
 }
 
 export default function Card({ post }: CardProps) {
+  const truncatedTitle = post.title.length > 30 ? post.title.slice(0, 30) + "..." : post.title;
+  const truncatedDescription = post.description.length > 80 ? post.description.slice(0, 80) + "..." : post.description;
+
   return (
     <Link href={`/pvi/${post.id}`} className="w-full h-full">
-    <div className="bg-azul-claro mx-auto w-full py-6 sm:py-8 pb-4 px-3 sm:px-4 flex flex-col items-center gap-3 sm:gap-4 rounded-xl hover:scale-105 transition-transform">
+    <div className="bg-azul-claro mx-auto w-full h-full py-6 sm:py-8 pb-4 px-3 sm:px-4 flex flex-col items-center gap-3 sm:gap-4 rounded-xl hover:scale-105 transition-transform">
         <div>
             <Image
                 src={post.principalImage}
@@ -22,13 +25,13 @@ export default function Card({ post }: CardProps) {
             />
         </div>
         <div className="flex flex-col items-center text-azul-escuro pb-2 sm:pb-4 text-sm sm:text-md font-normal text-center">
-            <p>{post.title}</p>
+            <p>{truncatedTitle}</p>
         </div>
         <div className="flex flex-col items-center text-azul-escuro pb-2 sm:pb-4 text-sm sm:text-md font-extrabold">
             <p>R$ {Number(post.price).toFixed(2)}</p>
         </div>
-        <div className="flex flex-col items-center text-azul-escuro pb-2 sm:pb-4 text-xs sm:text-md font-bold text-center line-clamp-2">
-            <p>{post.description}</p>
+        <div className="flex flex-col flex-grow items-center text-azul-escuro pb-2 sm:pb-4 text-xs sm:text-md font-bold text-center">
+            <p>{truncatedDescription}</p>
         </div>
     </div>
     </Link>
