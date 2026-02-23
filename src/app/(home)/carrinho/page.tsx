0b6057d492d3d9318c7882d4d7fe6c766f898885
marcosmvf22 <marcosviniciuskta123@gmail.com>
2026-02-22 22:22:39 -0300

    style: remove comentarios

--- src/app/(home)/carrinho/page.tsx
+++ src/app/(home)/carrinho/page.tsx
@@ -3,32 +3,6 @@
 import Carrinho from "@/src/components/carrinho";
 
 export default function PagCarrinho() {
-    // const carrinho = [
-    // {
-    //   id: 1,
-    //     image: "/logo/camisa1.png",
-    //     title: "Camiseta Confortável",
-    //     size: "M",
-    //     quantity: 2,
-    //     price: "R$ 49,90",
-    // },
-    // {
-    //   id: 2,
-    //     image: "/logo/camisa1.png",
-    //     title: "Camiseta Confortável",
-    //     size: "M",
-    //     quantity: 1,
-    //     price: "R$ 99,90",
-    // },
-    // {
-    //   id: 3,
-    //     image: "/logo/camisa1.png",
-    //     title: "Camiseta Confortável",
-    //     size: "M",
-    //     quantity: 1,
-    //     price: "R$ 42,90",
-    // },
-  // ];
   return (
     <Carrinho />
   );
